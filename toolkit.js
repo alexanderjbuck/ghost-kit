0c213d0695e8bb14c2916c76ca47ec327f4d22cf
alexanderjbuck <alexanderjbuck@gmail.com>
2024-01-05 22:22:49 -0500

    Update toolkit.js

--- toolkit.js
+++ toolkit.js
@@ -2,21 +2,31 @@
  * An extended toolkit for ghost sites.  Use at your own risk.
  * Go to your ghost site, and paste this file in your javascript console.
  * Run the functions as needed on the appropriate page.
+ * Repeated use may exceed the limits of the server and will result in a 429 error.
  * Alexander Buck
  */
 
-function postTitle(post) {
-	return post.getElementsByClassName("gh-content-entry-title")[0].innerText;
+/**
+ * Bulk delete articles on current page.  Deletion is limited by quantity.
+ * Run this when you have are viewing the list of posts you want to delete from.
+ * bulkDeleteAll(10) will delete 10 articles
+ */
+function bulkDeleteAll(limit) {
+	bulkDeleteByFilter(limit, p => true)
 }
 
 /**
  * Bulk delete articles on current page.  Deletion is limited by title and quantity.
  * Run this when you have are viewing the list of posts you want to delete from.
- * massDelete("Title", 10) will delete 10 articles with the title "Title"
+ * bulkDeleteByTitle("Title", 10) will delete 10 articles with the title "Title"
  */
-function bulkDeleteByTitle(title, limit) {
+function bulkDeleteByTitle(limit, title) {
+	bulkDeleteByFilter(limit, p => postTitle(p) === title)
+}
+
+function bulkDeleteByFilter(limit, filter) {
 	Array.from(document.getElementsByClassName("gh-post-list-plain-status"))
-      .filter(p => postTitle(p) === title)
+      .filter(filter)
       .filter((p,i) => i < limit)
       .forEach(p => {
         let attribs = p.attributes;
@@ -24,4 +34,8 @@ function bulkDeleteByTitle(title, limit) {
         let deleteUri = "https://navscout.ghost.io/ghost/api/admin/posts/?filter=id%3A%5B%27" + id + "%27%5D";
         fetch(deleteUri,{"method":"DELETE"})
       })
+}
+
+function postTitle(post) {
+	return post.getElementsByClassName("gh-content-entry-title")[0].innerText;
 }
